e8a6495a8f6ee02ef337d4c0c7025eded73d8696
Dominique <118232374+andompun@users.noreply.github.com>
2023-09-15 09:10:54 -0600

    week2 pushing

---
+++ week2/app/event-list.js
@@ -0,0 +1,15 @@
+export default function EventList() {
+    return (
+        <ul>
+            <li>
+                <h2> Community Picnic </h2>
+                <p> Date: September 16,2023 </p>
+                <p> Location: Central Park </p>
+            </li>
+            <li>
+                <h2></h2>
+            </li>
+        </ul>
+
+    )
+}
